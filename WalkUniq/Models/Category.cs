c231bccbd27250e8761a406911d2839d4dc6d84e
zeynel abidin <49595767+zagbey@users.noreply.github.com>
2023-10-31 14:37:21 +0300

    Data Annotations

--- WalkUniq/Models/Category.cs
+++ WalkUniq/Models/Category.cs
@@ -1,8 +1,13 @@
-﻿namespace WalkUniq.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WalkUniq.Models
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required] // not null
         public string Name { get; set; }
         public int DisplayOrder { get; set; }
     }
