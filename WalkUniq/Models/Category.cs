bc70f75e5e2d0ee25607af631b1001667de0b64e
zeynel abidin <49595767+zagbey@users.noreply.github.com>
2023-11-01 13:59:46 +0300

    Input Tag Helpers Added

--- WalkUniq/Models/Category.cs
+++ WalkUniq/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace WalkUniq.Models
 {
@@ -8,7 +9,9 @@ namespace WalkUniq.Models
         public int Id { get; set; }
 
         [Required] // not null
+        [DisplayName("Category Name")]
         public required string Name { get; set; }
+        [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
     }
 }
