508108bf0efebab06fe9b9ad44ab8df242408a6e
zeynel abidin <49595767+zagbey@users.noreply.github.com>
2023-11-01 12:04:32 +0300

    display all category

--- WalkUniq/Controllers/CategoryController.cs
+++ WalkUniq/Controllers/CategoryController.cs
@@ -14,7 +14,7 @@ namespace WalkUniq.Controllers
         public IActionResult Index()
         {
             List<Category> objCategoryList = _db.Categories.ToList();
-            return View();
+            return View(objCategoryList);
         }
     }
 }
