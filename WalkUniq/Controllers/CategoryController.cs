5a7fd877eeef2146bd0eaa29f1e46dd3d6db9d51
zeynel abidin <49595767+zagbey@users.noreply.github.com>
2023-11-01 11:43:22 +0300

    added Get all categories

--- WalkUniq/Controllers/CategoryController.cs
+++ WalkUniq/Controllers/CategoryController.cs
@@ -1,11 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WalkUniq.Data;
+using WalkUniq.Models;
 
 namespace WalkUniq.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
+            List<Category> objCategoryList = _db.Categories.ToList();
             return View();
         }
     }
