7d4a1c2c8bc29273dc34e59c87a530509d4511e5
zeynel abidin <49595767+zagbey@users.noreply.github.com>
2023-11-03 14:49:13 +0300

    Implement ICategoryRepository

---
+++ WalkUniq.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WalkUniq.Models;
+
+namespace WalkUniq.DataAccess.Repository.IRepository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        void Update(Category category);
+        void Save();
+    }
+}
