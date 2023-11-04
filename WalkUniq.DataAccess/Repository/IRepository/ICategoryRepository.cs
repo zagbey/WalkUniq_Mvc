d7476b18c5849c132b09c096948a7b1b555c374c
zeynel abidin <49595767+zagbey@users.noreply.github.com>
2023-11-04 19:35:10 +0300

    ICategoey repoda parametre adı değişti

--- WalkUniq.DataAccess/Repository/IRepository/ICategoryRepository.cs
+++ WalkUniq.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -9,7 +9,7 @@ namespace WalkUniq.DataAccess.Repository.IRepository
 {
     public interface ICategoryRepository : IRepository<Category>
     {
-        void Update(Category category);
+        void Update(Category obj);
         
     }
 }
