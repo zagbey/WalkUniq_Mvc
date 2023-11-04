9458f5a44908a4946504d04c58f9ab6732cf85a3
zeynel abidin <49595767+zagbey@users.noreply.github.com>
2023-11-04 19:48:23 +0300

    Produc Repository added UnitOfWork

--- WalkUniq.DataAccess/Repository/IRepository/IProductRepository.cs
+++ WalkUniq.DataAccess/Repository/IRepository/IProductRepository.cs
@@ -3,14 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WalkUniq.Models;
 
 namespace WalkUniq.DataAccess.Repository.IRepository
 {
-
-    public interface IUnitOfWork
+    public interface IProductRepository : IRepository<Product>
     {
-        ICategoryRepository Category { get; }
-        IProductRepository Product { get; }
-        void Save();
+        void Update(Product obj);
+        
     }
 }
---
+++ WalkUniq.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using WalkUniq.DataAccess.Data;
+using WalkUniq.DataAccess.Repository.IRepository;
+using WalkUniq.Models;
+
+namespace WalkUniq.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+
+        private ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) : base(db) 
+        {
+            _db = db;
+        }
+
+     
+
+        public void Update(Product obj)
+        {
+            _db.Products.Update(obj);
+        }
+    }
+}
