c034a4c0a032bcc21d6730110e41544138e86c24
zeynel abidin <49595767+zagbey@users.noreply.github.com>
2023-11-06 12:32:52 +0300

    Combine , create and edit to Upsert

--- WalkUniq/Areas/Admin/Controllers/ProductController.cs
+++ WalkUniq/Areas/Admin/Controllers/ProductController.cs
@@ -22,7 +22,7 @@ namespace WalkUniq.Areas.Admin.Controllers
            
             return View(objProductList);
         }
-        public IActionResult Create()
+        public IActionResult Upsert(int? id)
         {
             //kategorileri listeleme (projections)
             
@@ -35,12 +35,26 @@ namespace WalkUniq.Areas.Admin.Controllers
                 }),
                 Product = new Product()
             };
-            return View(productVM);
-        }
+            if (id==null || id ==0)
+            {
+                //CREATE
+                //create and return back to View 
+                return View(productVM);
+             
+             
+            }
+            else
+            {
+                //UPDATE
+                //update cause Id is present 
+                productVM.Product = _unitOfWork.Product.Get(u => u.Id == id);
+                return View(productVM);
+            }
+        }   
 
         //Create Product
         [HttpPost]
-        public IActionResult Create(ProductVM productVM)
+        public IActionResult Upsert(ProductVM productVM,IFormFile? file)
         {
             if (ModelState.IsValid)
             {
@@ -60,37 +74,7 @@ namespace WalkUniq.Areas.Admin.Controllers
             }
 
         }
-        public IActionResult Edit(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-            Product? productFromDb = _unitOfWork.Product.Get(u => u.Id == id);
-            //Product? productFromDb1 = _db.Categories.FirstOrDefault(u => u.Id == id);
-            //Product? productFromDb2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
-
-            if (productFromDb == null)
-            {
-                return NotFound();
-            }
-            return View(productFromDb);
-        }
-        [HttpPost]
-        public IActionResult Edit(Product obj)
-        {
-
-
-            if (ModelState.IsValid)
-            {
-                _unitOfWork.Product.Update(obj);
-                _unitOfWork.Save();
-                TempData["success"] = "Product edited successfully";
-                return RedirectToAction("Index");
-            }
-            return View();
-
-        }
+       
         public IActionResult Delete(int? id)
         {
             if (id == null || id == 0)
