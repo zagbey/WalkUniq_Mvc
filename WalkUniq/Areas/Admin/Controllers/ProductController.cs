6c1bf1ce5928289e5861e14cc673d43caa7e18aa
zeynel abidin <49595767+zagbey@users.noreply.github.com>
2023-11-06 15:07:12 +0300

    create product and displayimage

--- WalkUniq/Areas/Admin/Controllers/ProductController.cs
+++ WalkUniq/Areas/Admin/Controllers/ProductController.cs
@@ -12,9 +12,11 @@ namespace WalkUniq.Areas.Admin.Controllers
     public class ProductController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
-        public ProductController(IUnitOfWork unitOfWork)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
         }
         public IActionResult Index()
         {
@@ -58,8 +60,24 @@ namespace WalkUniq.Areas.Admin.Controllers
         {
             if (ModelState.IsValid)
             {
+                string wwwRootPath = _webHostEnvironment.WebRootPath;
+                if(file !=null)
+                {
+                    //RANDOM NAME FOR A IMAGE FILE
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                    //where we save that
+                    string productPath =Path.Combine(wwwRootPath,@"images\product" );
+
+                    using(var fileStream = new FileStream(Path.Combine(productPath,fileName),FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);
+                    }
+                    productVM.Product.ImageUrl = @"\images\product\" + fileName;
+                }
+                //oluşturulan ürünü kaydediyoruz
                 _unitOfWork.Product.Add(productVM.Product);
                 _unitOfWork.Save();
+                //başarı mesajı
                 TempData["success"] = "Product created successfully";
                 return RedirectToAction("Index");
             }
