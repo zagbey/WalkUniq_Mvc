7ac11a3540238c5b08799899d7ef53515c37c976
zeynel abidin <49595767+zagbey@users.noreply.github.com>
2023-11-07 11:38:45 +0300

    Home page and footer

--- WalkUniq/Areas/Customer/Controllers/HomeController.cs
+++ WalkUniq/Areas/Customer/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using WalkUniq.DataAccess.Repository.IRepository;
 using WalkUniq.Models;
 
 namespace WalkUniq.Areas.Customer.Controllers
@@ -8,15 +9,17 @@ namespace WalkUniq.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IUnitOfWork _unitOfWork;
+        public HomeController(ILogger<HomeController> logger,IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
+            return View(productList);
         }
 
         public IActionResult Privacy()
