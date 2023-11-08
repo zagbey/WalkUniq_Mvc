f95db5676faea3cf1d4551caec5a40d84f08a76f
zeynel abidin <49595767+zagbey@users.noreply.github.com>
2023-11-08 15:42:10 +0300

    SOME CHAGNGES FOR IDENTITY // understand what god added

--- WalkUniq/Program.cs
+++ WalkUniq/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
 
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
@@ -31,7 +31,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
