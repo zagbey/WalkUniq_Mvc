18ef346d5d23ee7a21e9035ee70567b71bb14b22
zeynel abidin <49595767+zagbey@users.noreply.github.com>
2023-11-08 21:18:40 +0300

    Roles created  in database

--- WalkUniq/Program.cs
+++ WalkUniq/Program.cs
@@ -3,6 +3,8 @@ using WalkUniq.DataAccess.Data;
 using WalkUniq.DataAccess.Repository;
 using WalkUniq.DataAccess.Repository.IRepository;
 using Microsoft.AspNetCore.Identity;
+using WalkUniq.Utility;
+using Microsoft.AspNetCore.Identity.UI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,11 +14,11 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddRazorPages();
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-
+builder.Services.AddScoped<IEmailSender,EmailSender>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
