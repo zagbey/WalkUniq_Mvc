e7f1dfaaf18cdd74903ffa1f66b2bd5192e5aaa3
zeynel abidin <49595767+zagbey@users.noreply.github.com>
2023-11-10 23:52:24 +0300

    Get Shoppingcart

--- WalkUniq/Areas/Customer/Controllers/CartController.cs
+++ WalkUniq/Areas/Customer/Controllers/CartController.cs
@@ -1,13 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using WalkUniq.DataAccess.Repository.IRepository;
+using WalkUniq.Models;
+using WalkUniq.Models.ViewModels;
 
 namespace WalkUniq.Areas.Customer.Controllers
 {
     [Area("customer")]
+    [Authorize] // only authorize user 
     public class CartController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+        public ShoppingCartVM ShoppingCartVM { get; set; }
+        public CartController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+
         public IActionResult Index()
         {
-            return View();
+            //USERID LAZIM
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            ShoppingCartVM = new()
+            {
+                ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId,
+                includeProperties: "Product")
+            };
+
+            return View(ShoppingCartVM);
         }
     }
 }
