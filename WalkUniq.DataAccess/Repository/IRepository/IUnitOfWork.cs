8bf3f157648f8e2f87fff810637f1d61c535934e
zeynel abidin <49595767+zagbey@users.noreply.github.com>
2023-11-10 11:33:16 +0300

    ShoppingCart Repository added

--- WalkUniq.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ WalkUniq.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -11,6 +11,7 @@ namespace WalkUniq.DataAccess.Repository.IRepository
     {
         ICategoryRepository Category { get; }
         IProductRepository Product { get; }
+        IShoppingCartRepository ShoppingCart { get; }
         void Save();
     }
 }
---
+++ WalkUniq.DataAccess/Repository/ShoppingCartRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using WalkUniq.DataAccess.Data;
+using WalkUniq.DataAccess.Repository.IRepository;
+using WalkUniq.Models;
+
+namespace WalkUniq.DataAccess.Repository
+{
+    public class ShoppingCartRepository : Repository<ShoppingCart>, IShoppingCartRepository
+    {
+
+        private ApplicationDbContext _db;
+        public ShoppingCartRepository(ApplicationDbContext db) : base(db) 
+        {
+            _db = db;
+        }
+
+     
+
+        public void Update(ShoppingCart obj)
+        {
+            _db.ShoppingCarts.Update(obj);
+        }
+    }
+}
