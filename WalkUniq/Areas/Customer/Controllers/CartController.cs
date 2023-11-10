451aa576ba18427748bab49c6da1809150fe924c
zeynel abidin <49595767+zagbey@users.noreply.github.com>
2023-11-10 23:18:38 +0300

    Shopping cart UI added

---
+++ WalkUniq/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WalkUniq.Areas.Customer.Controllers
+{
+    [Area("customer")]
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
