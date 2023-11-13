77919d32c73709912cea17a01e774c5ff9573408
zeynel abidin <49595767+zagbey@users.noreply.github.com>
2023-11-13 20:15:07 +0300

    Bug Solution and Logout

--- WalkUniq/Areas/Customer/Controllers/CartController.cs
+++ WalkUniq/Areas/Customer/Controllers/CartController.cs
@@ -203,7 +203,7 @@ namespace WalkUniq.Areas.Customer.Controllers
 
         public IActionResult Minus(int cartId)
         {
-            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId, tracked: true);
             if (cartFromDb.Count <= 1)
             {
                 //remove that from cart
@@ -224,7 +224,7 @@ namespace WalkUniq.Areas.Customer.Controllers
 
         public IActionResult Remove(int cartId)
         {
-            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId, tracked: true);
             HttpContext.Session.SetInt32(SD.SessionCart, _unitOfWork.ShoppingCart
                .GetAll(u => u.ApplicationUserId == cartFromDb.ApplicationUserId).Count() - 1);
             _unitOfWork.ShoppingCart.Remove(cartFromDb);
