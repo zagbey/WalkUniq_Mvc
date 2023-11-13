941e378add592024264b64a2da687db91f91199a
zeynel abidin <49595767+zagbey@users.noreply.github.com>
2023-11-13 14:35:16 +0300

    OrderList UI

--- WalkUniq.Models/ViewModels/OrderVM.cs
+++ WalkUniq.Models/ViewModels/OrderVM.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WalkUniq.Models
+namespace WalkUniq.Models.ViewModels
 {
     public class OrderVM
     {
--- WalkUniq/Areas/Admin/Controllers/OrderController.cs
+++ WalkUniq/Areas/Admin/Controllers/OrderController.cs
@@ -2,28 +2,34 @@
 using WalkUniq.DataAccess.Repository.IRepository;
 using WalkUniq.Models;
 
-namespace WalkUniq.Areas.Admin
+namespace WalkUniq.Areas.Admin.Controllers
 {
+    [Area("admin")]
     public class OrderController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
+
         public OrderController(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
         }
+
         public IActionResult Index()
         {
             return View();
         }
+
+
         #region API CALLS
+
         [HttpGet]
         public IActionResult GetAll()
         {
-            List<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApllicationUser").ToList();
+            List<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
             return Json(new { data = objOrderHeaders });
         }
 
-       
+
         #endregion
     }
 }
