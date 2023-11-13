740822a21a5d329f926507522286e62bb94673e4
zeynel abidin <49595767+zagbey@users.noreply.github.com>
2023-11-13 23:42:36 +0300

    Create View Component

--- WalkUniq/Areas/Customer/Controllers/HomeController.cs
+++ WalkUniq/Areas/Customer/Controllers/HomeController.cs
@@ -21,15 +21,7 @@ namespace WalkUniq.Areas.Customer.Controllers
 
         public IActionResult Index()
         {
-            //shopping carst session bug düzeltmesi 
-            var claimsIdentity = (ClaimsIdentity)User.Identity;
-            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
-
-            if (claim != null)
-            {
-                HttpContext.Session.SetInt32(SD.SessionCart,
-                _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == claim.Value).Count());
-            }
+           //shoppingcartviewcomponent e bak
             IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
             return View(productList);
         }
---
+++ WalkUniq/ViewComponents/ShoppingCartViewComponent.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using WalkUniq.DataAccess.Repository.IRepository;
+using WalkUniq.Utility;
+
+namespace WalkUniq.ViewComponents
+{
+    public class ShoppingCartViewComponent:ViewComponent
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ShoppingCartViewComponent(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            if (claim != null)
+            {
+                //data boş değilse database gitmemize gerek yok 
+                if (HttpContext.Session.GetInt32(SD.SessionCart) == null)
+                {
+                    HttpContext.Session.SetInt32(SD.SessionCart,
+                    _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == claim.Value).Count());
+                }
+
+                return View(HttpContext.Session.GetInt32(SD.SessionCart));
+            }
+            else
+            {
+                HttpContext.Session.Clear();
+                return View(0);
+            }
+        }
+
+    }
+}
+}
