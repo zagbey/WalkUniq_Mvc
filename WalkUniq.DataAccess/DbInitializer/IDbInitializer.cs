eb2984ef409477b491395edf9089d2132d37ee3a
zeynel abidin <49595767+zagbey@users.noreply.github.com>
2023-11-15 11:51:58 +0300

    DbInitilaizer

---
+++ WalkUniq.DataAccess/DbInitializer/IDbInitializer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WalkUniq.DataAccess.DbInitializer
+{
+    public interface IDbInitializer
+    {
+        //bu metod admin ve kullanıcı rolleri oluşturulmasından sorumlu 
+        void Initialize();
+    }
+}
